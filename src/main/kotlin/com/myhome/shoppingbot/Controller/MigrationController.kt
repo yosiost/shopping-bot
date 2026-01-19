dd476585ab59ff3e22a7a4f989576b0992342e08
Yosi Ostroviak <yosi.o@bizzabo.com>
2026-01-19 14:19:23 +0200

    add condition property to MigrationController.kt

--- src/main/kotlin/com/myhome/shoppingbot/Controller/MigrationController.kt
+++ src/main/kotlin/com/myhome/shoppingbot/Controller/MigrationController.kt
@@ -2,6 +2,7 @@ package com.myhome.shoppingbot.Controller
 
 import com.myhome.shoppingbot.Data.Voucher
 import com.myhome.shoppingbot.Repository.VoucherRepository
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/migration")
+@ConditionalOnProperty(name = ["app.migration.enabled"], havingValue = "true", matchIfMissing = false)
 class MigrationController (private val voucherRepository: VoucherRepository) {
 
     @PostMapping("/vouchers")
